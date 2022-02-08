0b4c00e0b57c7ad21229e21631f7a9bf3de308c2
Tianyu Lu <tianyu.lu@mail.utoronto.ca>
2022-02-07 21:46:58 -0500

    add pytorch dataloaders

--- protein_design/constants.py
+++ protein_design/constants.py
@@ -1,5 +1,5 @@
 AA = list("-ACDEFGHIKLMNPQRSTVWY")
 
-AA_IDX = {AA[i]:i for i in range(len(AA))}
+AA_IDX = {AA[i]: i for i in range(len(AA))}
 
-IDX_AA = {i:AA[i].upper() for i in range(len(AA))}
+IDX_AA = {i: AA[i].upper() for i in range(len(AA))}
---
+++ protein_design/pytorch_tools.py
@@ -0,0 +1,34 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class SequenceData(Dataset):
+    def __init__(self, X):
+        if not torch.is_tensor(X):
+            self.X = torch.from_numpy(X)
+        else:
+            self.X = X
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx]
+
+
+class SeqfuncData(Dataset):
+    def __init__(self, X, y):
+        if not torch.is_tensor(X):
+            self.X = torch.from_numpy(X)
+        else:
+            self.X = X
+        if not torch.is_tensor(y):
+            self.y = torch.from_numpy(y)
+        else:
+            self.y = y
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
