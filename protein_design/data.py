1771f520a86e0eb65ac0cf184bd609a8bb12c9e8
Tianyu Lu <tianyu@proteinqure.com>
2022-03-12 18:11:23 -0500

    ProteinData instead of SeqfuncData and SequenceData

--- protein_design/data.py
+++ protein_design/data.py
@@ -1,31 +1,20 @@
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
 
-class SequenceData(Dataset):
-    def __init__(self, X):
-        if not torch.is_tensor(X):
-            self.X = torch.from_numpy(X)
-        else:
-            self.X = X
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        return self.X[idx]
+def to_tensor(X) -> torch.tensor:
+    return X if torch.is_tensor(X) else torch.from_numpy(X)
 
 
-class SeqfuncData(Dataset):
+def to_numpy(X) -> np.ndarray:
+    return X.detach().cpu().numpy() if torch.is_tensor(X) else np.array(X)
+
+
+class ProteinData(Dataset):
     def __init__(self, X, y):
-        if not torch.is_tensor(X):
-            self.X = torch.from_numpy(X)
-        else:
-            self.X = X
-        if not torch.is_tensor(y):
-            self.y = torch.from_numpy(y)
-        else:
-            self.y = y
+        self.X = to_tensor(X)
+        self.y = to_tensor(y)
 
     def __len__(self):
         return len(self.X)
