f0e233e90b57ea9a455e30505e1407b3c7bbf34d
Tianyu Lu <tianyu@proteinqure.com>
2022-03-12 23:27:33 -0500

    initial commit of Attention module

--- protein_design/generative.py
+++ protein_design/generative.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.functional as F
 
 
 def KLLoss(mean, logvar):
@@ -77,3 +79,60 @@ class VAE(nn.Module):
     def reconstruct(self, x):
         recon = self.forward(x)
         return recon[0]
+
+
+class Attention(nn.Module):
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super(Attention, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.W_Q = nn.Linear(d_model, n_head * d_k)
+        self.W_K = nn.Linear(d_model, n_head * d_k)
+        self.W_V = nn.Linear(d_model, n_head * d_v)
+        self.W_O = nn.Linear(n_head * d_v, d_model)
+
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v):
+        batch, len_q, _ = q.size()
+        batch, len_k, _ = k.size()
+        batch, len_v, _ = v.size()
+
+        Q = self.W_Q(q).view([batch, len_q, self.n_head, self.d_k])
+        K = self.W_K(k).view([batch, len_k, self.n_head, self.d_k])
+        V = self.W_V(v).view([batch, len_v, self.n_head, self.d_v])
+
+        Q = Q.transpose(1, 2)
+        K = K.transpose(1, 2).transpose(2, 3)
+        V = V.transpose(1, 2)
+
+        # after transpose:
+        # Q: (batch, n_head, len_q, d_k)
+        # K: (batch, n_head, d_k, len_q)
+        # V: (batch, n_head, len_q, d_v)
+
+        attention = torch.matmul(Q, K)
+        # attention: (batch, n_head, len_q, len_q)
+
+        attention = attention / np.sqrt(self.d_k)
+
+        attention = F.softmax(attention, dim=-1)
+
+        output = torch.matmul(attention, V)
+        # output: (batch, n_head, len_q, d_v)
+
+        output = output.transpose(1, 2).reshape([batch, len_q, self.d_v * self.n_head])
+        # output: (batch, len_q, d_v * n_head)
+
+        output = self.W_O(output)
+
+        output = self.dropout(output)
+
+        output = self.layer_norm(output + q)
+
+        return output
